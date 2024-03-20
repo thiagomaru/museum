8270abce5f42050b8004832933b9b8bd7776a5cc
Thiago Maruyama <maruyama_thiago@hotmail.com>
2024-03-20 13:48:09 -0400

    alteracoes

---
+++ src/pages/App.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "reactstrap";
+import Headers from "./Headers";
+
+function App(props) {
+  return (
+    <div>
+      <Headers />
+      {/* <Button color="danger">Danger!</Button> */}
+    </div>
+  );
+};
+
+export default App;
